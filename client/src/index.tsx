05240611ecf66dbcd0703559c5f6bbbc0ba0a667
Andrey Konovalneko <andreinew@mail.ru>
2024-02-19 15:56:32 +0300

    chore: added mui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './styles/styles.scss';
 import App from './components/app/app'
 import reportWebVitals from './reportWebVitals';
 
