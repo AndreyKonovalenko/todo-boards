32788e74e992a6e35d457fdf0bb955c23efbc145
Andrey Konovalneko <andreinew@mail.ru>
2024-02-21 11:44:37 +0300

    chore: added login and register template pages

--- client/src/components/layout/layot.tsx
+++ client/src/components/layout/layot.tsx
@@ -1,15 +1,30 @@
 import { Outlet } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Container, Typography, Link } from '@mui/material';
 import HeaderBar from '../header-bar/header-bar';
 
+
+
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const Layout = (): JSX.Element => {
   return (
     <Container >
       <HeaderBar/>
       <Outlet />
+      <Copyright sx={{ mt: 5 }} />
     </Container>
-  
   );
 };
 
