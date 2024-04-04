e0a99cea16ea525c1eccd880a6651e37b8832edc
Andrey Konovalneko <andreinew@mail.ru>
2024-04-04 16:23:13 +0300

    chore: added username to header bar

--- client/src/components/header-bar/header-bar.tsx
+++ client/src/components/header-bar/header-bar.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { Person } from '@mui/icons-material';
 import { useMutation } from '@tanstack/react-query';
 import { useUserStore } from '../../services/user/user-store';
 import { Link as RouterLink } from 'react-router-dom';
@@ -11,10 +12,11 @@ import  Link  from '@mui/material/Link';
 import api from '../../utils/todo-boards-api';
 
 import { TO_MAIN } from '../../utils/route-constants';
+import { Paper, Stack, Typography } from '@mui/material';
 
 export default function HeaderBar() {
 
-   const user = useUserStore((state) => state.user);
+  const user = useUserStore((state) => state.user);
   const { reset } = useUserStore();
   const { mutate } = useMutation({
 		mutationFn: api.auth.logout,
@@ -29,12 +31,18 @@ export default function HeaderBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Link component={RouterLink} variant="h6" sx={{ flexGrow: 1 }} underline='none' to={TO_MAIN} color="inherit" onClick={()=> console.info('click')}>
-            Todo-boards
-          </Link>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link component={RouterLink} variant="h6"  underline='none' to={TO_MAIN} color="inherit" onClick={()=> console.info('click')}>
+              Todo-boards
+            </Link>
+          </Box>
           {user && (
-            <div>
-              <IconButton
+            <Stack direction="row" spacing={4}>
+              <Stack direction="row" sx={{alignItems: 'center'}} spacing={1} >
+                <Person color='inherit' fontSize='large'/>
+                <Typography variant='h6'>{user.username}</Typography>
+              </Stack>
+            <IconButton
                 size="large"
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
@@ -42,9 +50,9 @@ export default function HeaderBar() {
                 onClick={handleLogout}
                 color="inherit"
               >
-                <LogoutIcon/>
-              </IconButton>
-            </div>
+               <LogoutIcon fontSize='large'/>
+            </IconButton>
+            </Stack>
           )}
         </Toolbar>
       </AppBar>
--- client/src/pages/main-page/main-page.tsx
+++ client/src/pages/main-page/main-page.tsx
@@ -2,11 +2,12 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useUserStore } from '../../services/user/user-store';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
 import { TO_BOARDS } from '../../utils/route-constants';
 import api from '../../utils/todo-boards-api';
+import { Box, Container, Divider, Typography } from '@mui/material';
+import { Person } from '@mui/icons-material';
+// import { theme } from '../../styles/theme';
 
 const Item = styled(Paper)(({ theme }) => ({
 	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -22,17 +23,29 @@ const MainPage = () => {
 		queryFn: api.boards.fetchBoards,
 	});
 
+
+
 	return (
-		<Stack
-			mt={6}
-			direction={{ xs: 'column', sm: 'row' }}
-			spacing={{ xs: 1, sm: 2, md: 4 }}
-			justifyContent='center'>
-      <Link to={TO_BOARDS}>TO BOARDS</Link>
-			<Item>Item 1</Item>
-			<Item>Item 2</Item>
-			<Item>Item 3</Item>
-		</Stack>
+    <Container component='main' maxWidth='md'>
+      <Stack direction='row' sx={{marginTop: 8}} spacing={2}>
+        <Person fontSize="large" sx={(theme)=>({
+          color: theme.palette.text.secondary
+        })}/>
+        <Typography variant='h6'sx={(theme)=>({
+          color: theme.palette.text.secondary})}> Your boards</Typography>
+      </Stack>
+      <Divider/>
+		  <Stack
+        mt={6}
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        justifyContent='center'>
+        <Link to={TO_BOARDS}>TO BOARDS</Link>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>          
+    </Container>
 	);
 };
 export default MainPage;
