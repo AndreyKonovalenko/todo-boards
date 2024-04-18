c1b7e5075cdda87099ad6017d1458c06c5aed57c
AndreyKonovalenko <andreinew@mail.ru>
2024-04-19 01:00:11 +0300

    chore: added addBords and getBoards controllers

--- backend/services/boardService.ts
+++ backend/services/boardService.ts
@@ -1,14 +1,20 @@
-import { BoardModal, TBoard, TBoardDocument } from "../models";
-import { Types } from "mongoose";
+import { BoardModal, TBoard, TBoardDocument } from '../models';
+import { Types } from 'mongoose';
 
-export async function createBoard(board:TBoard): Promise<null | TBoardDocument>  {
-  const newBoard: TBoardDocument = await BoardModal.create(board)
-  console.log(newBoard)
-  return newBoard ? newBoard: null
+export async function createBoard(
+	board: TBoard
+): Promise<null | TBoardDocument> {
+	const newBoard: TBoardDocument = await BoardModal.create(board);
+	console.log(newBoard);
+	return newBoard ? newBoard : null;
 }
 
-export async function findBoardsByCreaterId(id:Types.ObjectId): Promise<null | Array<TBoardDocument>> {
-  const boards: Array<TBoardDocument> = await BoardModal.find({_id:id})
-  console.log(boards)
-  return boards? boards: null
+export async function findBoardsByCreaterId(
+	id: Types.ObjectId
+): Promise<null | Array<TBoardDocument>> {
+	const boards: Array<TBoardDocument> = await BoardModal.find({
+		creater_id: id,
+	});
+	console.log(boards);
+	return boards ? boards : null;
 }
