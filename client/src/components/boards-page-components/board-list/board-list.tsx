9b162aad782d5f65dc1d048a2dd7dcc6b0e7a263
Andrey Konovalneko <andreinew@mail.ru>
2024-07-03 15:37:58 +0300

    chore: added new colore to theme and theme types

--- client/src/components/boards-page-components/board-list/board-list.tsx
+++ client/src/components/boards-page-components/board-list/board-list.tsx
@@ -3,13 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 import CardComponent from '../card-component/card-component';
 const BoardList = (props: { title: string }) => {
-	const { spacing } = useTheme();
+	const { spacing, palette } = useTheme();
 	const { title } = props;
   const cardsMoch: any[] = [1,3,3,4];
   const cardsList = cardsMoch.length > 0 ? <List sx={{maxHeight: '40%', overflow: 'auto'}}>{cardsMoch.map(element => <ListItem key={uuidv4()}> <CardComponent /></ListItem>)}</List>: null;
 
 	return (
-		<Box sx={{flexShrink: 0, width: spacing(34) }}>
+		<Box sx={{flexShrink: 0, width: spacing(34), backgroundColor: palette.ochre.main }}>
         <Typography variant="h6" p={1}>{title}</Typography>
           {cardsList}
         <Button>
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -1,7 +1,27 @@
 import { createTheme } from "@mui/material";
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    ochre: Palette["primary"];
+  }
+  interface PaletteOptions {
+    ochre?: PaletteOptions["primary"];
+  }
+}
+
 export const theme = createTheme({
     typography: {
         fontFamily: "Chakra Petch",
-    }
+    },
+    palette:{
+      ochre: {
+        main: '#E3D026',
+        light: '#E9DB5D',
+        dark: '#A29415',
+        contrastText: '#242105',
+    },
+  }
 })
+ 
+
+console.log(theme)
