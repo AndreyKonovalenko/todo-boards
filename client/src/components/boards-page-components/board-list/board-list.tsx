e9ab9fa6879d2344bb7d9f6d28bee5b567fb375d
Andrey Konovalneko <andreinew@mail.ru>
2024-07-03 11:27:10 +0300

    chore: added draft card list component

--- client/src/components/boards-page-components/board-list/board-list.tsx
+++ client/src/components/boards-page-components/board-list/board-list.tsx
@@ -1,15 +1,21 @@
-import { Paper, useTheme } from '@mui/material';
+import { Box, Button, Paper, Typography, useTheme, List, ListItem } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
 import CardComponent from '../card-component/card-component';
 const BoardList = (props: { title: string }) => {
 	const { spacing } = useTheme();
 	const { title } = props;
+  const cardsMoch: any[] = [1,3,3,4];
+  const cardsList = cardsMoch.length > 0 ? <List sx={{maxHeight: '40%', overflow: 'auto'}}>{cardsMoch.map(element => <ListItem key={uuidv4()}> <CardComponent /></ListItem>)}</List>: null;
 
 	return (
-		<Paper sx={{ height: '100%', flexShrink: 0, width: spacing(34) }}>
-			{title}
-			<CardComponent />
-		</Paper>
+		<Box sx={{flexShrink: 0, width: spacing(34) }}>
+        <Typography variant="h6" p={1}>{title}</Typography>
+          {cardsList}
+        <Button>
+        + Add a card
+        </Button>
+		</Box>
 	);
 };
 
